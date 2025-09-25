d3d83bf08fc4e3d2ca4d1df8c5839a7e0ff0966c
Gabriel <gabrielrokssz@gmail.com>
2025-09-25 15:51:28 -0300

    aula1 adicionada

---
+++ aula3/ex/index.js
@@ -0,0 +1,2 @@
+nome = 'Gabriel Roks'
+console.log("Olá, meu nome é " + nome, "e estou aprendendo JavaScript às", 10, "da manhã.")
---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+// o método consele.log() imprime uma mensagem
+// em node.js não necessita de ponto e vírgula no final da linha
+console.log("Hello World"); // string
+console.log(123); // number
+console.log(1, 2, 3, 4, 5); // numeber
