659022bfd372fc064f72da2b123285a4adad1d51
Gabriel <gabrielrokssz@gmail.com>
2025-11-05 21:12:31 -0300

    mais aulas

--- aula_002/ex/index.js
+++ aula_002/ex/index.js
@@ -1,2 +1,2 @@
-nome = 'Gabriel Roks'
-console.log("Olá, meu nome é " + nome, "e estou aprendendo JavaScript às", 10, "da manhã.")
+let nome = 'Gabriel Roks'
+console.log("Olá, meu nome é ", nome, "e estou aprendendo JavaScript às", 10, "da manhã.")
