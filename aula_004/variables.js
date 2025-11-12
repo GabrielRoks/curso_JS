9431b96b26e6e43a5799ac1453deae8ae4f0d0a7
Gabriel <gabrielrokssz@gmail.com>
2025-11-12 00:21:39 -0300

    a

--- aula_004/variables.js
+++ aula_004/variables.js
@@ -1,3 +1,7 @@
-let nome = "Gabriel";
+let nome;
+nome = "Gabriel";
 var idade = 19; 
 console.log("Olá!, meu nome é", nome, "e eu tenho", idade, "anos.");
+//Nomes de variáveis não podem começar por números
+//Não podem conter espaços e nem traços
+// Utilizamos camelCase para nomear variáveis compostas
