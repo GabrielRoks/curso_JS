65a1e7a76f6e9888fd835a85663c5216f1f5638d
Gabriel <gabrielrokssz@gmail.com>
2025-11-13 20:16:57 -0300

    aula 4 e constantes na aula 5

--- aula_004/variables.js
+++ aula_004/variables.js
@@ -1,7 +1,8 @@
 let nome;
 nome = "Gabriel";
-var idade = 19; 
+let idade = 19; 
 console.log("Olá!, meu nome é", nome, "e eu tenho", idade, "anos.");
-//Nomes de variáveis não podem começar por números
-//Não podem conter espaços e nem traços
+// Nomes de variáveis não podem começar por números
+// Não podem conter espaços e nem traços
 // Utilizamos camelCase para nomear variáveis compostas
+// Não utilize var, utilize let
---
+++ aula_005/const.js
@@ -0,0 +1,5 @@
+const nome = "Gabriel";
+const primeiroNumero = 10;
+const segundoNumero = 5;
+const resultado = primeiroNumero * segundoNumero;
+console.log(nome, "tem", resultado, "reais.");
