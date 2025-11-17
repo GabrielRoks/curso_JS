f08dfe6b0ee12925010ad0e1ce16c9b7c738fda5
Gabriel <gabrielrokssz@gmail.com>
2025-11-16 21:23:39 -0300

    Exercicio sobre alert

---
+++ aula_009/js/script.js
@@ -0,0 +1,4 @@
+const fistNumber = prompt("Digite um número");
+const secondNumber = prompt("Digite outro número");
+const result = Number(fistNumber) + Number(secondNumber);
+alert(`A soma dos dois números é: ${result}`);
