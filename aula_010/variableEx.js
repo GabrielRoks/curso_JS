02589964c5ae44b5f6cfd3a0962dc58826baa989
Gabriel <gabrielrokssz@gmail.com>
2025-11-23 19:08:30 -0300

    Agora com arrays

--- aula_010/variableEx.js
+++ aula_010/variableEx.js
@@ -1,10 +1,14 @@
 let varA = "A";
 let varB = "B";
 let varC = "C";
-const varATemp = varA;
+// const varATemp = varA;
 
-varA = varB;
-varB = varC;
-varC = varATemp;
+// varA = varB;
+// varB = varC;
+// varC = varATemp;
 
-console.log(varA, varB, varC); // Output: B C A
+// console.log(varA, varB, varC); // Output: B C A
+
+// Outra forma:
+[varA, varB, varC] = [varB, varC, varA];
+console.log(varA, varB, varC);
