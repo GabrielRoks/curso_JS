4f5a9f19852ad88de1e91ecd581d3c5ed964897a
Gabriel <gabrielrokssz@gmail.com>
2025-11-23 19:23:15 -0300

    Indices

--- aula_011/strings.js
+++ aula_011/strings.js
@@ -1,2 +1,5 @@
 const umaString = "Olá \"Mundo\"";
 // A barra invertida (\) é usada para escapar caracteres especiais dentro de uma string.
+
+console.log(umaString[2]);
+// Retorna o indice 2 da string, que é a letra "á".
