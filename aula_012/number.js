020acf303c61d1842f490225d170e747895e5846
Gabriel <gabrielrokssz@gmail.com>
2025-11-25 20:12:53 -0300

    Aula 12 sobre numbers

---
+++ aula_012/number.js
@@ -0,0 +1,10 @@
+let number_01 = 1;
+let number_02 = 2.5;
+
+console.log(number_01.toString() + number_02.toString());
+let number_03 = 1500;
+console.log(number_03.toString(2));
+// transforma em binário
+let number_04 = 10.041391;
+console.log(number_04.toFixed(2));
+// limita o número para 2 casas decimais
