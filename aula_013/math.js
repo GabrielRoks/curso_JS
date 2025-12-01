b894656f03b7773e05c63059613075bf494d0fde
Gabriel <gabrielrokssz@gmail.com>
2025-12-01 19:38:49 -0300

    Metodo round

--- aula_013/math.js
+++ aula_013/math.js
@@ -4,3 +4,7 @@ let number_2 = Math.floor(number_1);
 console.log(number_2);
 let number_3 = Math.ceil(number_1);
 console.log(number_3);
+// arredonda para cima
+let number_4 = Math.round(number_1);
+console.log(number_4);
+// arredonda para o mais próximo
