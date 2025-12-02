3dab09647fc017be7a9f1ddb24aa7de5585caef0
Gabriel <gabrielrokssz@gmail.com>
2025-12-02 19:38:09 -0300

    Preciso lembrar dos ;

--- aula_014/array.js
+++ aula_014/array.js
@@ -1,3 +1,5 @@
 //               0          1       2
-const alunos = ['Gabriel', 'João', 'Daniel']
+const alunos = ['Gabriel', 'João', 'Daniel'];
 // Os arrays são indexaveis, tendo um indice para cada valor separado por vírgula
+alunos[0] = "Roks";
+// console.log(alunos[0]);
