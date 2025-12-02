c05aeca0200cc82ed6a69d554aecc67c5780b422
Gabriel <gabrielrokssz@gmail.com>
2025-12-02 19:45:35 -0300

    Adicionando algo nos array

--- aula_014/array.js
+++ aula_014/array.js
@@ -2,4 +2,7 @@
 const alunos = ['Gabriel', 'João', 'Daniel'];
 // Os arrays são indexaveis, tendo um indice para cada valor separado por vírgula
 alunos[0] = "Roks";
+// Reescreve um item do array
+alunos[3] = 'Maria';
+// adiciona algo no array
 // console.log(alunos[0]);
