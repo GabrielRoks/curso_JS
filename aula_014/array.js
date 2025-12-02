97aa109e3027db74a01447ce3d2eddf1fc2c35e6
Gabriel <gabrielrokssz@gmail.com>
2025-12-02 19:55:13 -0300

    metodo push

--- aula_014/array.js
+++ aula_014/array.js
@@ -1,11 +1,12 @@
 //               0          1       2
 const alunos = ['Gabriel', 'João', 'Daniel'];
 // Os arrays são indexaveis, tendo um indice para cada valor separado por vírgula
-alunos[alunos.length] = 'José';
-alunos[alunos.length] = 'Gabriele';
-alunos[alunos.length] = 'Matheus';
-alunos[alunos.length] = 'Carlos';
-console.log(alunos);
+alunos.push('Maria'); // adiciona algo ao fim do array
+// alunos[alunos.length] = 'José';
+// alunos[alunos.length] = 'Gabriele';
+// alunos[alunos.length] = 'Matheus';
+// alunos[alunos.length] = 'Carlos';
+// console.log(alunos);
 // console.log(alunos.length); // Retorna o tamanho do array
 // Reescreve um item do array
 // alunos[3] = 'Maria';
