a2e4cf3bb290a339776870fe5f1cbc6de660f0fe
Gabriel <gabrielrokssz@gmail.com>
2025-12-03 17:34:47 -0300

    Arrow function

--- aula_015/funcoes.js
+++ aula_015/funcoes.js
@@ -6,5 +6,10 @@ function soma(a, b){
     const resultado = a + b;
     return resultado;
 }
+
+// também podemos criar funções arrow function
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
 console.log(saudacao('Gabriel'));
 console.log(soma(2, 7));
