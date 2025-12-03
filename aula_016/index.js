159672aa4d5d5e065e93669d55f1b4fd0df92cdb
Gabriel <gabrielrokssz@gmail.com>
2025-12-03 18:40:10 -0300

    Adicionando funções em objetos

--- aula_016/index.js
+++ aula_016/index.js
@@ -1,5 +1,17 @@
-const pessoa01 = {
-    nome: 'Gabriel',
-    sobrenome: 'Roks',
-    idade: 19,
+function criaPessoa(nome, sobrenome, idade){
+    return{nome, sobrenome, idade};
 }
+const pessoa01 = criaPessoa('Gabriel', 'Roks', 19);
+const pessoa02 = criaPessoa('João', 'Rodrigues', 24);
+const pessoa03 = criaPessoa('Daniel', 'Vital', 17);
+const pessoa04 = criaPessoa('Matheus', 'Macedo', 18);
+
+const pessoa05 = {
+    nome: 'Gabriel',
+    sobrenome: 'Gomes',
+    idade: 19,
+    fala(){
+        console.log(`${this.nome} ${this.sobrenome} está falando oi`)
+    }
+}
+pessoa05.fala();
