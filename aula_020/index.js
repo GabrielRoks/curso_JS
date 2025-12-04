63b801158fdfae892bf66833fd54790fdd00f591
Gabriel <gabrielrokssz@gmail.com>
2025-12-03 21:01:57 -0300

    Operadores lógicos

---
+++ aula_020/index.js
@@ -0,0 +1,6 @@
+/*
+Operadores lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU -> Qualquer uma que for verdadeira, vai retorna true
+! -> NOT -> NÃO -> 
+*/
