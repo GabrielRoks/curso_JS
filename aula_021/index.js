e4f88acadccf02e7aa04f7f2474bb9830f9c3fd9
Gabriel <gabrielrokssz@gmail.com>
2025-12-05 12:20:43 -0300

    Avaliação de curto-cicuito com Falsy e Truthy

--- aula_021/index.js
+++ aula_021/index.js
@@ -9,3 +9,12 @@ FALSY VALUES:
 */
 // Avaliação de curto-cicuito:
 console.log("Gabriel" && 0 && true); // Retorna o primeiro valor Falsy
+function falaOi(){
+    return 'Oi';
+}
+const vaiExecutar = false;
+console.log(vaiExecutar && falaOi());
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preto';
+console.log(corPadrao); // Retorna o primeiro valor Truthy
