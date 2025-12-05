a8d8c99dfab28222e1b0e046959a9971973ade4e
Gabriel <gabrielrokssz@gmail.com>
2025-12-05 12:09:11 -0300

    Avaliação de curto-cicuito

---
+++ aula_021/index.js
@@ -0,0 +1,11 @@
+/*
+FALSY VALUES:
+- false
+- 0
+- "" ou '' (string vazia)
+- null
+- undefined
+- NaN
+*/
+// Avaliação de curto-cicuito:
+console.log("Gabriel" && 0 && true); // Retorna o primeiro valor Falsy
