2e0882406d4008646d0241145a5dfdae7428b0b2
Gabriel <gabrielrokssz@gmail.com>
2026-01-07 20:24:55 -0300

    Operação ternária

---
+++ aula_024/index.js
@@ -0,0 +1,5 @@
+// (condição) ? 'Valor para verdadeiro' : 'Valor para falso';
+const pontuacaoUsuario = 500;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+console.log(nivelUsuario);
