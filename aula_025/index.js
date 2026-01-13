22630eff54b432cd9424467d15e0333c4ad58516
Gabriel <gabrielrokssz@gmail.com>
2026-01-13 20:01:51 -0300

    Data e etc

--- aula_025/index.js
+++ aula_025/index.js
@@ -1,2 +1,10 @@
 const data = new Date('2026-01-13 19:54:59');
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth());
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log('ms', data.getMilliseconds());
+console.log('Dia semana', data.getDay());
 console.log(data.toString());
