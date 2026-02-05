272c67f2db0885f79d92a3a55c35e774488d6b71
Gabriel <gabrielrokssz@gmail.com>
2026-02-05 07:53:24 -0300

    laço de repetição com for

--- aula_029/index.js
+++ aula_029/index.js
@@ -1,4 +1,7 @@
-// i = index
-for (let i = 0; i <= 5; i++) {
+// // i = index
+// for (let i = 0; i <= 5; i++) {
+//     console.log(`linha ${i}`);
+// } // laço 'for' não termina com ponto e vígula
+for (let i = 500; i >= 400; i -= 10) {
     console.log(`linha ${i}`);
-} // laço 'for' não termina com ponto e vígula
+}
