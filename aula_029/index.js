daff4e1a6268cd881291a06370f470da0e723837
Gabriel <gabrielrokssz@gmail.com>
2026-02-05 08:08:36 -0300

    Aprendendo laço de repetição com for

--- aula_029/index.js
+++ aula_029/index.js
@@ -2,6 +2,10 @@
 // for (let i = 0; i <= 5; i++) {
 //     console.log(`linha ${i}`);
 // } // laço 'for' não termina com ponto e vígula
-for (let i = 500; i >= 400; i -= 10) {
-    console.log(`linha ${i}`);
+// for (let i = 500; i >= 400; i -= 10) {
+//     console.log(`linha ${i}`);
+// }
+const frutas = ['Uva', 'Morango', 'Banana']
+for (let i = 0; i <frutas.length; i++) {
+    console.log(frutas[i]);
 }
