00f61efa6c20995a88e637e77b6b2d8da08009a3
Gabriel <gabrielrokssz@gmail.com>
2026-03-16 19:37:27 -0300

    For of

---
+++ aula_032/index.js
@@ -0,0 +1,19 @@
+// for of
+
+// indices       12345
+const country = 'Japan';
+
+// // for classico
+// for (let i = 0; i < country.length; i++){
+//     console.log(country[i]);
+// }
+
+
+// for in
+// for (let i in country){
+//     console.log(country[i]);
+// }
+
+for (let valor of country){
+    console.log(valor);
+}
