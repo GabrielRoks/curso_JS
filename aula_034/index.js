20f26ce3ff5ec70258959382367bdd978b2cc9aa
Gabriel <gabrielrokssz@gmail.com>
2026-03-17 19:39:45 -0300

    While e Do While

--- aula_034/index.js
+++ aula_034/index.js
@@ -1,8 +1,8 @@
-// let controle = 0;
-// while (controle <= 10) {
-//     console.log(controle);
-//     controle++;
-// }
+let controle = 0;
+while (controle <= 10) {
+    console.log(controle);
+    controle++;
+}
 
 function random(min, max){
     const r = Math.random() * (max - min) + min;
