1aca8416df7701a25afa408473fdbd884ac4c68c
Gabriel <gabrielrokssz@gmail.com>
2026-03-24 17:04:59 -0300

    try, catch e throw

--- aula_039/index.js
+++ aula_039/index.js
@@ -1,6 +1,18 @@
-try{
-    console.log(naoExisto);
-} catch(err){
-    console.log('naoExisto não existe.');
-    console.log(err);
+function soma(x, y){
+    if (typeof x !== "number" || typeof y !== "number"){
+        throw new Error('x e y precisam ser números.');
+    }
+    return x + y;
 }
+try{
+    console.log(soma(1, "10"));
+} catch(err){
+    // console.log(err);
+    console.log('Algo inesperado ocorreu, não é sua culpa.')
+}
+// try{
+//     console.log(naoExisto);
+// } catch(err){
+//     console.log('naoExisto não existe.');
+//     console.log(err);
+// }
